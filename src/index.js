41e62e38c245836d3843390797e6f6bfd1ee46f5
yangming <472102644@qq.com>
2019-04-26 10:24:41 +0800

    replace

--- src/index.js
+++ src/index.js
@@ -3,17 +3,18 @@ const { join } = require('path')
 
 module.exports = function ({ types: t }) {
   let packageName
-  const importModules = []
-
   return {
     visitor: {
       ImportDeclaration: {
         enter(path) {
-          packageName = path.node.source.value
+          if (t.isStringLiteral(path.node.source)) {
+            packageName = path.node.source.value
+          }
         },
         exit(path, { opts: { libPath, noAlias } }) {
-          if (packageName === 'tcon') {
-            importModules.forEach(module => {
+          const sp = packageName.split('/')
+          if (sp[0] === 'tcon') {
+            sp.slice(1).forEach(module => {
               addSideEffect(path, libPath
                 ? `${noAlias ? '.' : '@'}/${join(libPath, `${module}.css`)}`
                 : join(packageName, 'dist', `${module}.css`)
@@ -21,11 +22,6 @@ module.exports = function ({ types: t }) {
             })
             path.remove()
           }
-        }
-      },
-      ImportSpecifier: {
-        enter(path) {
-          importModules.push(path.node.imported.name.toLowerCase())
         }
       }
     }
